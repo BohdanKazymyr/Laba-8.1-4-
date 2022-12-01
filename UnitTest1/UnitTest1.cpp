f438874674d86aae68b3ce0e264480b9566ee787
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-01 16:53:37 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 8.1(string)/Laba 8.1(string).cpp"
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str{102};
+			Count(str);
+			Change(str);
+		}
+	};
+}
